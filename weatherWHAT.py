0527568392f7cf5ef1ad66ec83cdb96c995fa6f3
Sean Danischevsky <32126021+seaniedan@users.noreply.github.com>
2020-01-29 10:23:45 +0000

    Update weatherWHAT.py

--- weatherWHAT.py
+++ weatherWHAT.py
@@ -75,7 +75,7 @@ def get_weather(latlong= latlong):
         latitude, longitude,
         extend= False, # default `False`
         lang= languages.ENGLISH, # default `ENGLISH`
-        units= units.AUTO, # default auto
+        #units= units.AUTO, # default auto
         exclude= [weather.MINUTELY],# weather.ALERTS] - default []
     )
     return forecast
