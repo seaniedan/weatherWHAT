4c15eba8b717355416c594df35dbbdb7b9f7e6d5
seaniedan <sean@seandanischevsky.com>
2024-04-23 19:14:32 +0100

    updated example API

--- example_api.py
+++ example_api.py
@@ -1,18 +1,15 @@
-# rename this file as api.py 
+# 1. rename this file as api.py 
 
-# fill in the client ID and secret from the met office website
+# 2. add your API key from the met office website
 # https://www.metoffice.gov.uk/services/data/met-office-weather-datahub
 # you'll need to sign in and subscribe to the 'hourly' weather data (free)
+key= "eyJ4NXQiO....etc....."
 
-# also fill in defauly latitude, longitude 
-# and add your name for Nominatim lookup of place names
+# 3. add your name for Nominatim lookup of place names
+# https://nominatim.org/ 
+user_agent= "Your Name"
 
- 
-clientId= "replaceWithYourClientIdString"
-clientSecret= "replaceWithYourClientSecret"
-
+# 4. fill in default latitude, longitude 
 # Your default latitude, longitude - you can get this from Google Maps, it will appear after the "@":
-lat,lon= 40.3234736,-72.3614222
-
-user_agent= "Your Name" #for geopy lookup via Nominatim
+lat, lon= 40.3234736,-72.3614222
 
